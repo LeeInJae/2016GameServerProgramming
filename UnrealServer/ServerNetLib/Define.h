8db0153905c03e01a304d58b866222aa573420d2
InJaeLee <LeeInJae@users.noreply.github.com>
2016-07-21 03:09:18 +0900

    Add ServerNetLib

---
+++ UnrealServer/ServerNetLib/Define.h
@@ -0,0 +1,74 @@
+#pragma once
+
+namespace NServerNetLib
+{
+	//서버의 정보들을 담을 구조체
+	//보통 ini파일로 관리한다.
+	const int MAX_IP_LEN		= 32;
+	const int MAX_PACKET_BODY_SIZE	= 1024;
+
+	struct ServerConfig
+	{
+		unsigned short	Port;
+		int				BackLogCount;
+
+		int				MaxClientCount;
+		int				ExtraClientCount;
+
+		short			MaxClientSockOptRecvBufferSize;
+		short			MaxClientSockOptSendBufferSize;
+		short			MaxClientRecvBufferSize;
+		short			MaxClientSendBufferSize;
+	};
+
+	struct RecvpacketInfo
+	{
+		int SessionIndex		= 0;
+		short PacketID			= 0;
+		short PacketBodySize	= 0;
+		char* p_RefData			= 0;
+	};
+
+	/*
+	struct RecvpacketInfo2
+	{
+	int SessionIndex = 0;
+	short PacketID = 0;
+	short PacketBodySize = 0;
+	char* p_RefData = 0;
+	};
+	*/
+
+	enum class SOCKET_CLOSE_CASE : short
+	{
+		SESSION_POOL_EMPTY					= 1,
+		SELECT_ERROR						= 2,
+		SOCKET_RECV_ERROR					= 3,
+		SOCKET_ERCV_BUFFER_PROCESS_ERROR	= 4,
+		SOCKET_SEND_ERROR					= 5,
+		FORCING_CLOSE						= 6,
+	};
+
+	enum class PACKET_ID : short
+	{
+		NTF_SYS_CONNECT_SESSINO = 2,
+		NTF_SYS_CLOSE_SESSION	= 3,
+	};
+
+	//요녀석에 한해서 1바이트 정렬(push , pop 스택 구조로 되어있다)
+	//push, pop 안해주면, 설정한 값으로 모든 구조체가 정렬되게된다.
+#pragma pack(push, 1)
+	struct PacketHeader
+	{
+		short ID;
+		short BodySize;
+	};
+
+	const int PACKET_HEADER_SIZE = sizeof(PacketHeader);
+
+	struct PacketSysCloseSession : PacketHeader
+	{
+		int SockFD;
+	};
+#pragma pack(pop)
+}
---
+++ UnrealServer/ServerNetLib/TcpNetwork.h
@@ -0,0 +1,83 @@
+#pragma once
+
+#define FD_SETSIZE 1024
+
+#pragma comment(lib, "ws2_32")
+
+#include <WinSock2.h>
+#include <WS2tcpip.h>
+
+#include <vector>
+#include <deque>
+#include <unordered_map>
+#include "ITcpNetwork.h"
+#include "ClientSession.h"
+#include "ServerNetErrorCode.h"
+
+namespace NServerNetLib
+{
+	class TcpNetwork : public ITcpNetwork
+	{
+	public:
+		TcpNetwork();
+		virtual ~TcpNetwork();
+
+		NET_ERROR_CODE Init(const ServerConfig* p_Config, ILog* p_Logger) override;
+
+		NET_ERROR_CODE SendData(const int SessionIndex, const short PacketID, const short Size, const char* p_Msg) override;
+
+		void Run() override;
+
+		RecvpacketInfo GetPacketInfo() override;
+
+		void Release() override;
+
+		int ClientSessionPoolSize() override
+		{
+			return static_cast<int>(m_ClientSessionPool.size());
+		}
+
+		void ForcingClose(const int SessionIndex) override;
+
+	protected:
+		NET_ERROR_CODE InitServerSocket();
+		NET_ERROR_CODE BindListen(short Port, int BacklogCount);
+
+		int AllocClientSessionIndex();
+		void ReleaseSessionIndex(const int Index);
+
+		void CreateSessionPool(const int MaxClientCount);
+		NET_ERROR_CODE NewSession();
+		void SetSockOption(const SOCKET FD);
+		void ConnectedSession(const int SessionIndex, const int FD, const char* p_IP);
+
+		void CloseSession(const SOCKET_CLOSE_CASE CloseCase, const SOCKET SockFD, const int SessionIndex);
+
+		NET_ERROR_CODE RecvSocket(const int SessionIndex);
+		NET_ERROR_CODE RecvBufferProcess(const int SessionIndex);
+		void AddPacketQueue(const int SessionIndex, const short PacketID, const short BodySize, char* p_DataPos);
+
+		void RunProcessWrite(const int SessionIndex, const SOCKET FD, fd_set& Write_set);
+		NetError FlushSendBuff(const int SessionIndex);
+		NetError SendSocket(const SOCKET FD, const char* p_Msg, const int Size);
+
+		bool RunCheckSelectResult(const int Result);
+		void RunCheckSelectClients(fd_set& Read_set, fd_set& Write_set);
+		bool RunProcessReceive(const int SessionIndex, const SOCKET FD, fd_set& Read_set);
+	protected:
+		ServerConfig m_Config;
+		SOCKET m_ServerSockFD;
+
+		fd_set m_ReadFDs;
+		size_t m_ConnectedSessionCount = 0;
+
+		int64_t m_ConnectSeq = 0;
+
+		std::vector<ClientSession> m_ClientSessionPool;
+		std::deque<int> m_ClientSessionPoolIndex;
+
+		std::deque<RecvpacketInfo> m_PacketQueue;
+
+		ILog* mp_RefLogger;
+	};
+}
